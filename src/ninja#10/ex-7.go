264eb031767466e053140b7d9759bb736995af1e
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2020-04-20 15:27:32 +0100

    channels with multiple goroutines -- x tasks

---
+++ src/ninja#10/ex-7.go
@@ -0,0 +1,38 @@
+// write a program that
+// launches 10 goroutines
+// each goroutine adds 10 numbers to a channel
+// pull the numbers off the channel and print them
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	c := make(chan int)
+	defer receive(c)
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		send(c)
+		wg.Done()
+	}
+	wg.Wait()
+}
+
+func send(c chan int) <-chan int {
+	go func(c chan<- int) {
+		for j := 0; j < 10; j++ {
+			c <- j
+		}
+		close(c)
+	}(c)
+	return c
+}
+func receive(c <-chan int) {
+	for n := range c {
+		fmt.Println(n)
+	}
+}
